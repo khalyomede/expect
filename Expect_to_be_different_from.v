8f03b10ce53dd38ee4aaa016bdb81638220ca9fe
khalyomede <khalyomede@gmail.com>
2025-06-07 21:43:06 +0200

    fix wrong assertion error message for to_be_different_from assertion

--- Expect_to_be_different_from.v
+++ Expect_to_be_different_from.v
@@ -1,7 +1,7 @@
 module expect
 
 pub fn (expectation Expectation[T]) to_be_different_from[T, U](expected_value U) Expectation[T] {
-    assert expectation.actual_value != expected_value, 'Expected "${expectation.actual_value}" to be different from zero value of type ${expected_value}'
+    assert expectation.actual_value != expected_value, 'Expected "${expectation.actual_value}" to be different from "${expected_value}"'
 
     return expectation
 }
