deb3ea3a79872df976345411d3e8538c81585e01
khalyomede <khalyomede@gmail.com>
2025-06-07 21:42:01 +0200

    fix property access inconsistancy

--- Expect_to_not_be_empty.v
+++ Expect_to_not_be_empty.v
@@ -3,8 +3,8 @@ module expect
 pub fn (expectation Expectation[T]) to_not_be_empty[T]() Expectation[T] {
     $for field in T.fields {
         $if field.typ is String ||
-            field.type is Array ||
-            field.type is Map {
+            field.typ is Array ||
+            field.typ is Map {
             assert expectation.actual_value.len > 0, 'Expected "${expectation.actual_value}" to not be empty'
         } $else {
             assert false, 'Expected "${expectation.actual_value}" to be either String, Array of Map'
